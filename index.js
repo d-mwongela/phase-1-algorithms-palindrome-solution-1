849e354f0f1dd7556fa62655cdecedbac406257c
dennis00095 <dennis.mwongela@student.moringaschool.com>
2022-08-30 13:28:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverseString(word) {
+  return word.split("").reverse().join("");
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
+function isPalindrome(word) {
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // compare the reversed string to the input
+  return word === reversedWord;
+}
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
